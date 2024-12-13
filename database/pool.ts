9afaa039ccb5ebce06fd4dff55857d3f7f78c13f
Bob2006 <sbobohoni4@gmail.com>
2024-12-13 11:26:25 +0500

    new database

--- database/pool.ts
+++ database/pool.ts
@@ -4,10 +4,10 @@ import { config } from "dotenv";
 config();
 
 export const pool = new Pool({
-  user: "db_nutrition_5wf9_user",
-  password: "KbbbE4Dlq7pa5bT3RJQRpxrDJ8ejyklo",
+  user: "usr_nutrition",
+  password: "w3hsdAPxU7MSKzsHVSdSPKJpTN0HD5C0",
   port: 5432,
-  database: "db_nutrition_5wf9",
-  host: "dpg-con2a9kf7o1s73fc9f9g-a.frankfurt-postgres.render.com",
+  database: "db_nutrition_8kef",
+  host: "dpg-ctdt3ilumphs7398aia0-a.frankfurt-postgres.render.com",
   ssl: true,
 });
